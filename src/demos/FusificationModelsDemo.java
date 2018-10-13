357986e0d5a3919771ae46c31c6704ddc16fd7f3
Zabdiel García <zabadiel071@gmail.com>
2018-10-12 21:57:22 -0500

    Demo

---
+++ src/demos/FusificationModelsDemo.java
@@ -0,0 +1,32 @@
+package demos;
+
+import fuzzyfication.models.FuzzyVariable;
+import fuzzyfication.models.Label;
+import fuzzyfication.models.Point;
+
+import java.util.ArrayList;
+
+/**
+ * Demo básico de la implementación de puntos, etiquetas y variables difusas
+ */
+public class FusificationModelsDemo {
+    public static void main(String[] args) {
+        ArrayList<Point> pointsLabel1 = new ArrayList<>();
+        ArrayList<Point> pointsLabel2 = new ArrayList<>();
+
+        for (byte i = 0; i < 10; i++){
+            pointsLabel1.add(new Point( i, (byte)(8*i)));
+            pointsLabel2.add(new Point( i, (byte)(3*i)));
+        }
+
+        Label label1 = new Label("Alto",pointsLabel1);
+        Label label2 = new Label("Bajo",pointsLabel2);
+
+        ArrayList<Label> labels = new ArrayList<>();
+        labels.add(label1);
+        labels.add(label2);
+
+        FuzzyVariable fuzzyVariable = new FuzzyVariable("Var1", labels);
+        System.out.println(fuzzyVariable);
+    }
+}
