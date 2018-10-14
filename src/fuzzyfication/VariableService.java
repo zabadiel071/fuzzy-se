e7a8af06fc70186af3c4cbeccba136d1824b8719
Zabdiel García <zabadiel071@gmail.com>
2018-10-14 15:15:03 -0500

    Documentación sobre variableService

--- src/fuzzyfication/VariableService.java
+++ src/fuzzyfication/VariableService.java
@@ -13,10 +13,13 @@ import java.util.Arrays;
  * Clase para carga de los datos desde variables.json
  * Es necesario añadir la libreria Gson
  * Se pueden utilizar los archivos master_variables y variables_index que vienen por defecto en el repositorio
- * en caso de que no se quiera instalar la libreria. 
+ * en caso de que no se quiera instalar la libreria.
  *
  * Comentar esta clase si está causando conflictos al compilar.
  *
+ * Para añadir Gson en IntelliJ :
+ * ***** https://www.jetbrains.com/help/idea/library.html#download-libraries-from-maven
+ * ***** Utilizando : com.google.code.gson:gson:2.8.5 como repositorio de Maven
  */
 public class VariableService {
 
